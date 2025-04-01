3194da7c8368b9c69eab3cdbf80f97883f5a627a
Shasanka-shaw1 <shasankashaw5@gmail.com>
2025-04-01 18:11:23 +0530

    shasanka

--- heelo.java
+++ heelo.java
@@ -1,6 +1,7 @@
 // dnekndendjknedekjd
 public class heelo{
     public static void main(String args[]){
-        System.out.println("Aman Pandey");
+        System.out.println("Aman ");
+        System.out.println("Aman Shasanka");
     }
 }
