0c8db9b4f956da62ec1453794bcf985f1e4491f6
Aman Pandey <pandeyaman1904@gmail.com>
2025-04-01 18:11:38 +0530

    newmessae

--- heelo.java
+++ heelo.java
@@ -1,6 +1,8 @@
 // dnekndendjknedekjd
 public class heelo{
     public static void main(String args[]){
-        System.out.println("Aman Pandey");
+        System.out.println("XYZ");
+        System.out.println("djnejkde");
+        System.out.println("debd");
     }
 }
